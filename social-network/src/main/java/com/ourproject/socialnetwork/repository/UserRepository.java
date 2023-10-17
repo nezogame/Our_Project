de499727a9ca40b9e4186f9a43b0f52dfc11d878
Denys <denys.hudymov@gmail.com>
2023-10-17 23:47:18 +0300

    UserRepository created that extends MongoRepository implements methods from a package and methods find user by username

---
+++ social-network/src/main/java/com/ourproject/socialnetwork/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ourproject.socialnetwork.repository;
+
+import com.ourproject.socialnetwork.entity.User;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
+
+    Optional<User> findUserByUserName(String name);
+
+}
