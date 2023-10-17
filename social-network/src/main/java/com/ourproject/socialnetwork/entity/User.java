6b3d2fec03491ab7274c2c20f513732b40d1add6
Denys <denys.hudymov@gmail.com>
2023-10-18 00:15:14 +0300

    User entity that represents document in Mongodb

---
+++ social-network/src/main/java/com/ourproject/socialnetwork/entity/User.java
@@ -0,0 +1,59 @@
+package com.ourproject.socialnetwork.entity;
+
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.stereotype.Component;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Component
+@Document(collection ="Users")
+public class User {
+    @Id
+    @Field("user_id")
+    private Long userId;
+    @Field("access_token")
+    @Indexed(unique = true)
+    private String accessToken;
+    @Field("post_id")
+    private Long postId;
+    @Field("chat_id")
+    private Long chatId;
+    @Field("user_name")
+    private String userName;
+    @Field("mail")
+    @Indexed(unique = true)
+    private String mail;
+    @Field("sex")
+    private boolean sex;
+    @Field("photo_ptr")
+    private String photoPtr;
+    @Field("followers")
+    private Integer followers;
+    @Field("following")
+    private Integer following;
+    @Field("user_bio")
+    private String userBio;
+    @Field("date_of_birth")
+    private LocalDate dob;
+    @Field("join_date")
+    private LocalDate joinDate;
+    @Field("password")
+    @Indexed(unique = true)
+    private String password;
+}
