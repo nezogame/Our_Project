74af75aa11be6a2555e7ae46133dc5490e27dd2c
Denys <denys.hudymov@gmail.com>
2023-10-24 00:06:33 +0300

    The UserDto contain all fields of entity

--- social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/UserDto.java
+++ social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/UserDto.java
@@ -1,8 +1,19 @@
 package com.ourproject.socialnetwork.model;
 
+import java.time.LocalDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 public record UserDto(
+        Long userId,
+        String accessToken,
         String userName,
+        String password,
+        String mail,
         String sex,
+        Long postId,
+        Long chatId,
+        String photoPtr,
         String followers,
         String following,
         String userBio,
