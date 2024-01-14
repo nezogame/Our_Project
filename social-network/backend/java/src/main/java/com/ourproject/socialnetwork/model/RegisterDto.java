5952928d118c86e1c5babb9aa97fd75f4858cc61
Denys <denys.hudymov@gmail.com>
2024-01-15 00:10:21 +0200

    I added validation for some fields to ensure the valid data representation

--- social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/RegisterDto.java
+++ social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/RegisterDto.java
@@ -1,10 +1,20 @@
 package com.ourproject.socialnetwork.model;
 
 import java.util.Date;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 public record RegisterDto(
+        @NotBlank(message = "Username can't be empty")
         String username,
+        @NotEmpty(message = "Email can't be empty")
+        @Email
         String mail,
+        @NotNull
+        @Size(min = 4, max = 16, message = "password should have at least 4 characters and maximum 16" )
         String password,
         boolean sex,
         Date dob
