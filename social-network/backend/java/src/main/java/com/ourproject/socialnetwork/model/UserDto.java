0e5d91ed186c0f610db0d23b9e415debfefd304b
Denys <denys.hudymov@gmail.com>
2024-01-15 00:04:44 +0200

    I changed type and added validation for some fields

--- social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/UserDto.java
+++ social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/UserDto.java
@@ -1,17 +1,28 @@
 package com.ourproject.socialnetwork.model;
 
-import java.time.LocalDate;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Field;
+import com.ourproject.socialnetwork.enums.Gender;
+import com.ourproject.socialnetwork.entity.Chat;
+import com.ourproject.socialnetwork.entity.Post;
+import java.util.List;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public record UserDto(
         Long userId,
+        @NotBlank(message = "Username can't be empty")
         String username,
+        @NotNull
+        @Size(min = 4, max = 16, message = "password should have at least 4 characters and maximum 16" )
         String password,
+        @Email
+        @NotEmpty(message = "Email can't be empty")
         String mail,
-        String sex,
-        Long postId,
-        Long chatId,
+        Gender sex,
+        List<Post> postId,
+        List<Chat> chatId,
         String photoPtr,
         String followers,
         String following,
