9953b233a6d104f6a814c1202643b127902ce3ce
Denys <denys.hudymov@gmail.com>
2024-01-14 23:38:13 +0200

    add validation with message to the fields

--- social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/AuthenticationDto.java
+++ social-network/backend/java/src/main/java/com/ourproject/socialnetwork/model/AuthenticationDto.java
@@ -1,7 +1,14 @@
 package com.ourproject.socialnetwork.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotBlank;
+
 public record AuthenticationDto(
+        @NotBlank(message = "Username can't be empty")
         String username,
+        @NotNull
+        @Size(min = 4, max = 16, message = "password should have at least 4 characters and maximum 16")
         String password
 ) {
 }
