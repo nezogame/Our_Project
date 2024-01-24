3207a01dbb055647a0a2d1d36c655ab59cb2c48d
Denys <denys.hudymov@gmail.com>
2024-01-25 00:03:41 +0200

    I added two method for create and read chats

--- social-network/backend/java/src/main/java/com/ourproject/socialnetwork/service/ChatService.java
+++ social-network/backend/java/src/main/java/com/ourproject/socialnetwork/service/ChatService.java
@@ -1,6 +1,11 @@
 package com.ourproject.socialnetwork.service;
 
+import com.ourproject.socialnetwork.entity.Chat;
+import com.ourproject.socialnetwork.mapper.ChatMapper;
+import com.ourproject.socialnetwork.model.ChatDto;
+import com.ourproject.socialnetwork.model.SaveChatDto;
 import com.ourproject.socialnetwork.repository.ChatRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +14,19 @@ import org.springframework.stereotype.Service;
 public class ChatService {
     private final ChatRepository chatRepository;
 
-    private final SequenceGeneratorService userSequenceService;
+    private final SequenceGeneratorService chatSequenceService;
+
+    public List<ChatDto> findAllChats() {
+        var chats = chatRepository.findAll();
+        return chats.stream()
+                .map(ChatMapper.INSTANCE::chatToChatDto)
+                .toList();
+    }
+
+    public ChatDto createChat(SaveChatDto saveChatDto) {
+        Chat chat = ChatMapper.INSTANCE.saveChatDtoToChat(saveChatDto);
+        chat.setChatId(chatSequenceService.generateSequence(Chat.SEQUENCE_NAME));
+        chat = chatRepository.save(chat);
+        return ChatMapper.INSTANCE.chatToChatDto(chat);
+    }
 }
